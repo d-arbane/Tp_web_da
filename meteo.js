46ed2c2597f3cd10f348982680661c7548184d5b
d-arbane <64135320+d-arbane@users.noreply.github.com>
2021-02-10 19:58:35 +0100

    mise a jour

---
+++ meteo.js
@@ -0,0 +1,30 @@
+
+/* 
+function showCity()
+{    console.log(villeCourante)
+    //alert(villeCourante);      
+
+}
+let villeCourante="Lyon";
+*/
+
+
+
+function showCity() {
+ 
+    let nomVilleSelectionnee = document.getElementById("menu").value;
+  
+   
+    let cities = document.getElementsByClassName("city");
+    let l=cities.length;
+    for (i=0; i<l; i++) {
+      if (cities[i].id == nomVilleSelectionnee ) {
+        //garder l'element visible -> 
+        cities[i].style.display="block";
+          
+      } else {
+        //cacher lelement -> 
+        cities[i].style.display="none";
+      }
+       }
+       }
